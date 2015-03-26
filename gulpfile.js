ad59501b0ef7e1e9914bcb9765b29afb8177d77b
mcibique <mcibique@hotmail.com>
2015-03-26 19:48:00 +0000

    Basic watchers for js and html.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 
-gulp.task('serve', function () {
+var sources = {
+  html: ['./src/**/*.html'],
+  js: ['./src/scripts/**/*.js']
+};
+
+gulp.task('connect', function () {
   connect.server({
     root: ['src', '.'],
     port: 8082,
     livereload: true
   });
 });
+
+gulp.task('html', function () {
+  gulp.src(sources.html)
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+  gulp.src(sources.js)
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(sources.html, ['html']);
+  gulp.watch(sources.js, ['js']);
+});
+
+gulp.task('serve', ['connect', 'watch']);
