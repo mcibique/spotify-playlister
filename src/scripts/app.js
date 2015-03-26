f2e37907801ac69e69981a5265aebbdfa594674b
mcibique <mcibique@hotmail.com>
2015-03-26 20:59:55 +0000

    Added spotify authentication.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,54 @@
-(function (ng) {
+(function (ng, $) {
+  'use strict';
 
-  ng.module('playlister', [])
-    .run(function ($log) {
+  ng.module('playlister', ['ui.router', 'playlister.auth', 'playlister.spotify-credentials'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('login', {
+          url: '/login/',
+          templateUrl: 'views/login.html',
+          controller: 'LoginController'
+        })
+        .state('playlists', {
+          url: '/',
+          templateUrl: 'views/playlists.html'
+        });
+
+      $urlRouterProvider.otherwise('/');
+    })
+    .run(function ($log, $window, $rootScope, $state, auth) {
       $log.debug('Playlister is running.');
+
+      var search = $window.location.search;
+      var matches = /code=([^&]*)/g.exec(search);
+      if (matches) {
+        var code = matches[1];
+        if (code) {
+          auth.setKey(code);
+          $window.location.href = '/';
+        }
+      }
+    })
+    .controller('LoginController', function ($scope, spotifyCredentials) {
+      var params = {
+        client_id: spotifyCredentials.clientId,
+        response_type: 'code',
+        redirect_uri: spotifyCredentials.redirectUri,
+        scope: spotifyCredentials.scopes.join(' '),
+        show_dialog: false
+      };
+
+      $scope.spotifyAuthorizeUrl = spotifyCredentials.authorizeUrl + '/?' + $.param(params);
+    })
+    .controller('LogoutController', function ($scope, $state, auth) {
+      $scope.logout = function () {
+        auth.clearKey();
+        $state.go('login');
+      };
+
+      $scope.isLoggedIn = function () {
+        return !!auth.getKey();
+      };
     });
 
-})(angular);
+})(angular, jQuery);
---
+++ src/scripts/spotify-credentials.js
@@ -0,0 +1,12 @@
+(function (ng) {
+  'use strict';
+
+  ng.module('playlister.spotify-credentials', [])
+    .constant('spotifyCredentials', {
+      authorizeUrl: 'https://accounts.spotify.com/authorize',
+      clientId: '<< insert valid client ID >>',
+      redirectUri: '<< insert valid redirect URI >>',
+      scopes: ['playlist-read-private', 'playlist-modify-public', 'playlist-modify-private']
+    });
+
+})(angular);
