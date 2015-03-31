13ab57bed1ae9adb63a096332e3b354207ea7249
mcibique <mcibique@hotmail.com>
2015-03-31 23:40:48 +0100

    Added sass support.

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,58 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var clean = require('gulp-clean');
+var autoprefixer = require('gulp-autoprefixer');
 
 var sources = {
+  tmp: ['./.tmp'],
   html: ['./src/**/*.html'],
-  js: ['./src/scripts/**/*.js']
+  js: ['./src/scripts/**/*.js'],
+  scss: ['./src/styles/main.scss']
 };
+
+gulp.task('clean', function () {
+  return gulp.src(sources.tmp, {
+      read: false
+    })
+    .pipe(clean());
+});
 
 gulp.task('connect', function () {
   connect.server({
-    root: ['src', '.'],
+    root: ['src', '.', '.tmp'],
     port: 8082,
     livereload: true
   });
 });
 
 gulp.task('html', function () {
-  gulp.src(sources.html)
+  return gulp.src(sources.html)
     .pipe(connect.reload());
 });
 
 gulp.task('js', function () {
-  gulp.src(sources.js)
+  return gulp.src(sources.js)
+    .pipe(connect.reload());
+});
+
+gulp.task('styles', function () {
+  return gulp.src(sources.scss)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer({
+      cascade: false,
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./.tmp/styles/'))
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
   gulp.watch(sources.html, ['html']);
   gulp.watch(sources.js, ['js']);
+  gulp.watch(sources.scss, ['styles']);
 });
 
-gulp.task('serve', ['connect', 'watch']);
+gulp.task('serve', ['connect', 'styles', 'watch']);
